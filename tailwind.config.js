23ef272d5a491004594bb8ff21e907d3369f2b21
Santiago Garcia <just.b000ringsh1t@gmail.com>
2022-07-29 17:40:09 -0300

    instalacion tailwind y prettier

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        bevietnam: ['Be Vietnam', 'sans-serif']
+      },
+      colors: {
+        'natalia-blue': {
+          400: '#2653F1',
+          500: '#1D3BA7'
+        },
+        'natalia-lightblue': {
+          400: '#26C0F1'
+        },
+        'natalia-gray': {
+          300: '#D9D9D9',
+          400: '#484848'
+        }
+      }
+    },
+    container: {
+      center: true,
+      padding: '1em'
+    }
+  },
+  plugins: []
+};
