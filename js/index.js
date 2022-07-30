402d58ee8c7920fd087e1436a3f2e953b740e1c5
Santiago Garcia <just.b000ringsh1t@gmail.com>
2022-07-30 13:48:38 -0300

    menu responsive basico

--- js/index.js
+++ js/index.js
@@ -1,55 +1,70 @@
-$(() => {
-  const PRODUCTOS = [
-    {
-      id: 1,
-      img: 'assets/products/producto1.jpg',
-      titulo: 'Producto 1 mega nashe',
-      descripcion:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
-      precio: 700,
-    },
-    {
-      id: 1,
-      img: 'assets/products/producto1.jpg',
-      titulo: 'Producto 1 mega nashe',
-      descripcion:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
-      precio: 700,
-    },
-    {
-      id: 1,
-      img: 'assets/products/producto1.jpg',
-      titulo: 'Producto 1 mega nashe',
-      descripcion:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
-      precio: 700,
-    },
-    {
-      id: 1,
-      img: 'assets/products/producto1.jpg',
-      titulo: 'Producto 1 mega nashe',
-      descripcion:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
-      precio: 700,
-    },
-    {
-      id: 1,
-      img: 'assets/products/producto1.jpg',
-      titulo: 'Producto 1 mega nashe',
-      descripcion:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
-      precio: 700,
-    },
-    {
-      id: 1,
-      img: 'assets/products/producto1.jpg',
-      titulo: 'Producto 1 mega nashe',
-      descripcion:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
-      precio: 700,
-    },
-  ];
+const PRODUCTOS = [
+  {
+    id: 1,
+    img: 'assets/products/producto1.jpg',
+    titulo: 'Producto 1 mega nashe',
+    descripcion:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
+    precio: 700,
+  },
+  {
+    id: 1,
+    img: 'assets/products/producto1.jpg',
+    titulo: 'Producto 1 mega nashe',
+    descripcion:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
+    precio: 700,
+  },
+  {
+    id: 1,
+    img: 'assets/products/producto1.jpg',
+    titulo: 'Producto 1 mega nashe',
+    descripcion:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
+    precio: 700,
+  },
+  {
+    id: 1,
+    img: 'assets/products/producto1.jpg',
+    titulo: 'Producto 1 mega nashe',
+    descripcion:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
+    precio: 700,
+  },
+  {
+    id: 1,
+    img: 'assets/products/producto1.jpg',
+    titulo: 'Producto 1 mega nashe',
+    descripcion:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
+    precio: 700,
+  },
+  {
+    id: 1,
+    img: 'assets/products/producto1.jpg',
+    titulo: 'Producto 1 mega nashe',
+    descripcion:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit Eaque placeat  labore...',
+    precio: 700,
+  },
+];
 
+$(() => {
+  // MENU HAMBURGUESA RESPONSIVE
+  const $menuBoton = $('#menu-boton');
+  const $window = $(window);
+
+  $menuBoton.on('click', () => {
+    if ($menuBoton.hasClass('open')) $menuBoton.removeClass('open');
+    else $menuBoton.addClass('open');
+  });
+
+  $window.on('resize', () => {
+    if ($window.width() < 620) return;
+    if ($menuBoton.hasClass('open')) $menuBoton.removeClass('open');
+  });
+
+  // CARGAR ULTIMOS PRODUCTOS
   const $listaUltimosProductos = $('#last-products');
   PRODUCTOS.map(producto => {
     $listaUltimosProductos.append(
@@ -69,6 +84,7 @@ $(() => {
     );
   });
 
+  // CARGAR ULTIMAS OFERTAS
   const $listaUltimasOfertas = $('#last-offers');
   PRODUCTOS.map(producto => {
     $listaUltimasOfertas.append(
